c345242f9a2eac8453754084cbb2fa58c81e7fc0
vinay ganesh.d <vinayganeshdxj@gmail.com>
2017-03-30 03:14:23 +0530

    Route Fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  get 'meabout' , to: 'pages#about'
+  get 'about' , to: 'pages#about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
