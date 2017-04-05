798fca63e9fad745fa98e94aa9afd57c1c1d62db
vinay ganesh.d <vinayganeshdxj@gmail.com>
2017-04-05 12:14:05 +0530

    Restrict User Action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
 
   before_action :set_article, only: [:show, :edit, :update, :destroy]
-
+  before_action :require_user, except: [:index, :show]
 
   def new
     @article = Article.new
